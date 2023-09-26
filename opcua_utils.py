c28e3ae080fc5d8ee9b6ecd2a060cf43257b6992
Alice Donini <alice.donini@inaf.it>
2023-09-26 10:01:30 +0100

    add while true connection

--- opcua_utils.py
+++ opcua_utils.py
@@ -64,28 +64,16 @@ class OPCUAConnection():
         try:
             async with Client(url=self.url) as client:
                 #logger.debug('Connected to ', self.url)
+                while True:
+                    for element in self.dpsList:
+                        node = "ns=" + element["NS"] + ";s=" + element["Name"]
+                        var = client.get_node(node)
+                        self.listOfWSNode.append(var)
 
-                for element in self.dpsList:
-                    node = "ns=" + element["NS"] + ";s=" + element["Name"]
-                    var = client.get_node(node)
-                    self.listOfWSNode.append(var)
-
-                logger.debug("Reading all values")
-                #read_start_time = time.time()
-                for nid in self.listOfWSNode:
-                    #print("Reading node: ", nid)
-                    try:
-                        var = await nid.read_value()
-                        # create more readable name
-                        node_name = nid.nodeid.to_string().rpartition('.')[2][:-2].replace("_", " ")
-                        if node_name == "Time":
-                            var = var.strip()  # remove any leading/trailing spaces
-                            if len(var) < 6:
-                                var = var.zfill(6)  # pad with leading zeros if necessary
-                        self.WSDPValues[node_name] = var
-                    except Exception:
-                        logger.debug(f"Couldn't read node {nid}")
-                        logger.debug(f"Trying reading node {nid} a second time.")
+                    logger.debug("Reading all values")
+                    #read_start_time = time.time()
+                    for nid in self.listOfWSNode:
+                        #print("Reading node: ", nid)
                         try:
                             var = await nid.read_value()
                             # create more readable name
@@ -96,18 +84,30 @@ class OPCUAConnection():
                                     var = var.zfill(6)  # pad with leading zeros if necessary
                             self.WSDPValues[node_name] = var
                         except Exception:
-                            logger.error(f"Second attempt failed for node {nid}")
-                            if nid == "ns=2;s=Unit_WS.WS.Monitoring.Time.Time_v" or nid == "ns=2;s=Unit_WS.WS.Monitoring.Date.Date_v":
-                                logger.error(f"Couldn't read node {nid}. Not possible to have timestamp, so avoid entering it to the DB.")
-                                return {}
-                            self.WSDPValues[nid.nodeid.to_string().rpartition('.')[2][:-2].replace("_", " ")] = None
+                            logger.debug(f"Couldn't read node {nid}")
+                            logger.debug(f"Trying reading node {nid} a second time.")
+                            try:
+                                var = await nid.read_value()
+                                # create more readable name
+                                node_name = nid.nodeid.to_string().rpartition('.')[2][:-2].replace("_", " ")
+                                if node_name == "Time":
+                                    var = var.strip()  # remove any leading/trailing spaces
+                                    if len(var) < 6:
+                                        var = var.zfill(6)  # pad with leading zeros if necessary
+                                self.WSDPValues[node_name] = var
+                            except Exception:
+                                logger.error(f"Second attempt failed for node {nid}")
+                                if nid == "ns=2;s=Unit_WS.WS.Monitoring.Time.Time_v" or nid == "ns=2;s=Unit_WS.WS.Monitoring.Date.Date_v":
+                                    logger.error(f"Couldn't read node {nid}. Not possible to have timestamp, so avoid entering it to the DB.")
+                                    return {}
+                                self.WSDPValues[nid.nodeid.to_string().rpartition('.')[2][:-2].replace("_", " ")] = None
                             # if date or time value are not readable exit
-                #read_end_time = time.time()
-                #logger.debug(f"Reading values took {read_end_time - read_start_time} seconds")
-                #end_time = time.time()
-                #logger.debug(f"Total elapsed time: {end_time - start_time} seconds")
-                #print("Set of WS data: ", self.WSDPValues)
-                return self.WSDPValues
+                    #read_end_time = time.time()
+                    #logger.debug(f"Reading values took {read_end_time - read_start_time} seconds")
+                    #end_time = time.time()
+                    #logger.debug(f"Total elapsed time: {end_time - start_time} seconds")
+                    print("Set of WS data: ", self.WSDPValues)
+                    return self.WSDPValues
         except Exception as e:
             logger.error(f"Can not connect to OPCUA. Error: {e}")
 
